c6588cc9e1c450f835970d14cf083ddac907861c
Isaque Alves <isaquealves@gmail.com>
2019-07-12 09:48:34 -0300

    Add basic test for get profile route

--- test_instacollector_app.py
+++ test_instacollector_app.py
@@ -3,6 +3,7 @@ import json
 import unittest
 
 from unittest.mock import patch
+import instaloader
 
 import boto3
 from moto import mock_s3
@@ -23,11 +24,13 @@ class InstacollectorTestCase(unittest.TestCase):
     def test_load_instagram_profile(self):
         with patch('instaloader.Instaloader.download_profiles') as mock_insta:
             with patch('instaloader.Instaloader.download_post') as mock_post:
-                mock_insta.return_value.ok = True
+                mock_insta.return_value = True
                 mock_post.return_value = {'post_data':'Post'}
 
                 response = self.client().get(f"/{self.instagram_user}")
         self.assertEqual(response.status_code, 200)
 
-
-
+    def test_get_profile_from_s3(self):
+        with mock_s3():
+            response = self.client().get(f"/{self.instagram_user}/profile")
+            self.assertEqual(response.status_code, 200)
